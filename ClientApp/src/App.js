10b939895e8c565d2e1aab308b3c822b77669488
Vazhadval <vazhadvalishvili@gmail.com>
2020-09-23 13:48:50 +0400

    login page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Redirect, Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
+import { Login } from './components/Login';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route path='/Login' component={Login} />
+                <PrivateRoute exact path='/Home' component={Home} />
+                <PrivateRoute path='/fetch-data' component={FetchData} />
+            </Layout>
+        );
+    }
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    return (
+        <Route {...rest} render={(props) =>
+            true ? <Component {...props} />
+                : <Redirect to='/Login' />
+        } />
+    );
+}
+
+
---
+++ ClientApp/src/components/Login.js
@@ -0,0 +1,62 @@
+﻿import React, { Component } from 'react';
+
+export class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            username: "",
+            password: "",
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="d-flex justify-content-around">
+                <form className="col-md-5">
+                    <div className="row justify-content-between">
+                        <h2 className="text-center">Log in</h2>
+                        <div>
+                            <button className="btn btn-secondary btn-sm">Sign up</button>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Username"
+                            required="required"
+                            value={this.state.username}
+                            onChange={(e) => this.handleUsernameOnChange(e)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                            required="required"
+                            value={this.state.password}
+                            onChange={(e) => this.handlePasswordOnChange(e)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary btn-block">Log in</button>
+                    </div>
+                </form>
+            </div>
+        );
+
+    }
+
+    handleUsernameOnChange(e) {
+        this.setState({ username: e.target.value });
+    }
+
+    handlePasswordOnChange(e) {
+        this.setState({ password: e.target.value });
+    }
+}
+
